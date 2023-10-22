38dcfe22a926a34e318458ea487942962bb1ef04
oferisak <doctoroots@gmail.com>
2023-10-22 11:58:31 +0300

    changed the check-sex to also look at YCOUNTS. updated the way the male-threshold is set by taking two SD below the mean F for '0' class

--- lib/plink_functions.R
+++ lib/plink_functions.R
@@ -8,9 +8,9 @@ vcf_to_plink_bfiles<-function(plink_path,vcf_file,bfiles_prefix,build){
 # run plink's check-sex command on the bfiles generated after the split-x command was ran. 
 # the male and female thresh should be set according to the dataset (so should be ran twice, once to get the F stats and second to set the threshs)
 # gender is calculated by if F<female_thresh than female (2) and if F higher than male threshold than male (1)
-run_plink_check_sex<-function(plink_path,bfiles_prefix,female_thresh=0.2,male_thresh=0.8){
+run_plink_check_sex<-function(plink_path,bfiles_prefix,female_thresh=0.2,male_thresh=0.8,max_y_for_female=5,min_y_for_male=1){
   # first get the F stats
-  check_sex_command<-glue('{plink_path} --check-sex {female_thresh} {male_thresh} --bfile {bfiles_prefix} --out {bfiles_prefix}')
+  check_sex_command<-glue('{plink_path} --check-sex ycount {female_thresh} {male_thresh} {max_y_for_female} {min_y_for_male} --bfile {bfiles_prefix} --out {bfiles_prefix}')
   system(check_sex_command)
 }
 
@@ -22,7 +22,7 @@ parse_plink_check_sex_output<-function(check_sex_output_file){
 
 check_for_gender_outliers<-function(check_sex_output){
   # calculate Z score
-  check_sex_output<-check_sex_output%>%group_by(SNPSEX)%>%reframe(id=IID,F=F,z=(F-mean(F))/sd(F))
+  check_sex_output<-check_sex_output%>%group_by(SNPSEX)%>%reframe(id=IID,F=F,YCOUNT=YCOUNT,z=(F-mean(F))/sd(F))
   check_sex_output<-check_sex_output%>%mutate(outlier=abs(z)>1.96)
   return(check_sex_output)
 }
